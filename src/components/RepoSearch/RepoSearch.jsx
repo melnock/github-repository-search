d04a6ddfe272b1de9bd3e06a77ddda420ac22a61
melnock <melissa@vidmob.com>
2020-09-30 21:50:29 -0400

    begin set up for the filtering by language on the frontend

--- src/components/RepoSearch/RepoSearch.jsx
+++ src/components/RepoSearch/RepoSearch.jsx
@@ -10,7 +10,24 @@ const RepoSearch = () => {
   const [searchError, setSearchError] = useState(null);
   const [sortOption, setSortOption] = useState('');
   const [searchResults, setSearchResults] = useState([]);
+  const [searchResultLanguages, setSearchResultLanguages] = useState({});
+  const [selectedSearchResultLanguage, setSelectedSearchResultLanguage] = useState(null);
+
   console.log(searchValue);
+  // when we get a new batch of results, we should determine which languages are available to us to filter by
+  const extractLanguagesFromResults = () => {
+    const languagesObj = {};
+    searchResults.forEach(result => {
+      if (!languagesObj[result.language]) {
+        languagesObj[result.language] = result.language
+      }
+    });
+    setSearchResultLanguages(languagesObj);
+  };
+
+  const filterSearchResultsByLanguage = () => {
+    return searchResults.filter(result => result.language === selectedSearchResultLanguage);
+  };
 
   const getSearchResults = async () => {
     if (searchValue.length) {
@@ -18,16 +35,25 @@ const RepoSearch = () => {
       try {
         const resp = await octokit.request('GET /search/repositories',
   {q: searchValue, sort: sortOption});
-        console.log(resp.data);
+
         setSearchResults(resp.data.items);
+        // below: reset the error and selectedSearchResultLanguage on a new search
         setSearchError(null);
+        selectedSearchResultLanguage(null);
+        extractLanguagesFromResults()
       } catch (error) {
-        setSearchError('something went wrong with retrieving your search results');
+        console.error(error);
+        setSearchError('Something went wrong with retrieving your search results. Please try again.');
       }
     } else {
-      setSearchError('must enter a valid search value');
+      // verify that the user has input search criteria
+      setSearchError('Please enter a valid search value');
     }
   };
+
+  const filteredSearchResults = selectedSearchResultLanguage ?
+    filterSearchResultsByLanguage() :
+    searchResults;
 
   return (
     <div className="repo-search-page">
@@ -37,8 +63,10 @@ const RepoSearch = () => {
         setSortOption={setSortOption}
         getSearchResults={getSearchResults}
         searchError={searchError}
+        searchResultLanguages={searchResultLanguages}
+        setSelectedSearchResultLanguage={setSelectedSearchResultLanguage}
       />
-      <RepoSearchBody searchResults={searchResults}/>
+      <RepoSearchBody searchResults={filteredSearchResults}/>
     </div>
   );
 };
